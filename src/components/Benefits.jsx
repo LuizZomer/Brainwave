1e129061a210fd5eebdccd9a7a031d4c3de3cdbd
Luiz Zomer <lipezomer@gmail.com>
2024-10-03 19:29:48 -0300

    fix: benefits card image

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -17,7 +17,7 @@ const Benefits = () => {
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] border border-n-6 rounded-[2rem] rounded-tr-[4.5rem]"
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] "
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
